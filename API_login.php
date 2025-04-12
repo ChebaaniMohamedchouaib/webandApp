8705e30cfdac57c1e48705ac0a54b08737d0d030
ChebaaniMohamedChouaib <chebaanimohamed@gmail.com>
2025-04-12 11:33:00 +0100

    api

--- API_login.php
+++ API_login.php
@@ -1,6 +1,6 @@
 <?php
 include 'conection.php';
-header('Access-Control-Allow-Origin: http://localhost:8081');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Content-Type: application/json; charset=utf-8');
 $data = file_get_contents("php://input");
@@ -10,15 +10,17 @@ if (isset($data)) {
     $password = $data->password;
 
     // Prepare and execute the SQL statement
-    $stmt = $conn->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
+    $stmt = $conn->prepare("SELECT * FROM user WHERE `email` = ? AND   `password` = ?");
     $stmt->bind_param("ss", $email, $password);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         // User found, return success message
-
-        echo json_encode(["message" => "Login successful." ]);
+        $sql = "SELECT * FROM user WHERE `email` = '$email' AND   `password` = '$password'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        echo json_encode(["message" => "Login successful." , "user" => $row['user_name'] , "email" => $row['email'] , "datepre" => $row['date_pregnancy'] ]);
     } else {
         // User not found, return error message
         echo json_encode(["message" => "Invalid email or password."]);
